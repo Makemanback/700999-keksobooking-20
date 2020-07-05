5fc67a88c2731512123bebecb3cfe845adece264
makemanback <makemanback@gmail.com>
2020-07-05 19:45:36 +0200

    настроил принятие данных с сервера

--- js/map.js
+++ js/map.js
@@ -3,30 +3,12 @@
 (function () {
   window.utils.makeElementsDisabled(window.htmlSelectors.mapFeatures);
   window.utils.makeElementsDisabled(window.htmlSelectors.mapFiltersSelects);
-  window.utils.hideElements(window.renderedSelectors.mapPinsCollection);
-
-  var onClickOpenCard = function (element, data) {
-    element.addEventListener('click', function () {
-      window.card.removeCard();
-      window.card.renderMapCard(data);
-    });
-  };
-
-  var openCard = function () {
-    window.pin.ads.forEach(function (item, index) {
-      onClickOpenCard(window.renderedSelectors.mapPinsCollection[index], item);
-    });
-  };
-
-  openCard();
 
   var makePageActive = function () {
-
     window.htmlSelectors.map.classList.remove('map--faded');
     window.htmlSelectors.adForm.classList.remove('ad-form--disabled');
     window.utils.makeElementsAvailable(window.htmlSelectors.mapFeatures);
     window.utils.makeElementsAvailable(window.htmlSelectors.mapFilter);
-    window.utils.showElements(window.renderedSelectors.mapPinsCollection);
   };
 
   var makeElementActive = function (evt) {
